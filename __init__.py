1bf9d0b96154587730ab80aa84069f868d65eeba
Shammi G <shammig.sng@gmail.com>
2025-11-19 09:55:43 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,18 +1,21 @@
 import os
 from .Simple_Readable_Metadata_MAX_SG import SimpleReadableMetadataMAXSG
 from .Simple_Readable_Metadata_SG import SimpleReadableMetadataSG
-from. Simple_Readable_Metadata_Text_Viewer_SG import SimpleReadableMetadataTextViewerSG
+from .Simple_Readable_Metadata_Text_Viewer_SG import SimpleReadableMetadataTextViewerSG
+from .Simple_Readable_Metadata_Save_Text_SG import SimpleReadableMetadataSaveTextSG
 
 NODE_CLASS_MAPPINGS = {
     "SimpleReadableMetadataMAXSG": SimpleReadableMetadataMAXSG,
     "SimpleReadableMetadataSG": SimpleReadableMetadataSG,
     "Simple Readable Metadata Text Viewer-SG": SimpleReadableMetadataTextViewerSG,
+    "SimpleReadableMetadataSaveTextSG": SimpleReadableMetadataSaveTextSG
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SimpleReadableMetadataMAXSG": "Simple Readable Metadata MAX-SG",
     "SimpleReadableMetadataSG": "Simple Readable Metadata-SG",
-    "Simple Readable Metadata Text Viewer-SG": "Simple Readable Metadata 🧾 Text Viewer-SG"
+    "Simple Readable Metadata Text Viewer-SG": "Simple Readable Metadata 🧾 Text Viewer-SG",
+    "SimpleReadableMetadataSaveTextSG": "Simple Readable Metadata Save Text-SG"
 }
 
 WEB_DIRECTORY = "./web"
