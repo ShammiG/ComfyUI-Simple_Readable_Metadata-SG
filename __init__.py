b2cee55488a980ae4a43aa43deb1e843ce6b7d06
Shammi G <shammig.sng@gmail.com>
2025-12-06 20:58:13 +0530

    Video Support Added

--- __init__.py
+++ __init__.py
@@ -3,19 +3,22 @@ from .Simple_Readable_Metadata_MAX_SG import SimpleReadableMetadataMAXSG
 from .Simple_Readable_Metadata_SG import SimpleReadableMetadataSG
 from .Simple_Readable_Metadata_Text_Viewer_SG import SimpleReadableMetadataTextViewerSG
 from .Simple_Readable_Metadata_Save_Text_SG import SimpleReadableMetadataSaveTextSG
+from .Simple_Readable_Metadata_VIDEO_SG import SimpleReadableMetadataVideoSG
 
 NODE_CLASS_MAPPINGS = {
     "SimpleReadableMetadataMAXSG": SimpleReadableMetadataMAXSG,
     "SimpleReadableMetadataSG": SimpleReadableMetadataSG,
     "Simple Readable Metadata Text Viewer-SG": SimpleReadableMetadataTextViewerSG,
-    "SimpleReadableMetadataSaveTextSG": SimpleReadableMetadataSaveTextSG
+    "SimpleReadableMetadataSaveTextSG": SimpleReadableMetadataSaveTextSG,
+    "SimpleReadableMetadataVideoSG": SimpleReadableMetadataVideoSG
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SimpleReadableMetadataMAXSG": "Simple Readable Metadata MAX-SG",
     "SimpleReadableMetadataSG": "Simple Readable Metadata-SG",
     "Simple Readable Metadata Text Viewer-SG": "Simple Readable Metadata 🧾 Text Viewer-SG",
-    "SimpleReadableMetadataSaveTextSG": "Simple Readable Metadata Save Text-SG"
+    "SimpleReadableMetadataSaveTextSG": "Simple Readable Metadata Save Text-SG",
+    "SimpleReadableMetadataVideoSG": "Simple Readable Metadata (VIDEO)-SG"
 }
 
 WEB_DIRECTORY = "./web"
