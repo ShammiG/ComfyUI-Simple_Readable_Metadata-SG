2a5372da4556c82d25bdfcc161b52082782a6fe1
Shammi G <shammig.sng@gmail.com>
2026-01-04 10:40:40 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,17 @@ from .Simple_Readable_Metadata_SG import SimpleReadableMetadataSG
 from .Simple_Readable_Metadata_Text_Viewer_SG import SimpleReadableMetadataTextViewerSG
 from .Simple_Readable_Metadata_Save_Text_SG import SimpleReadableMetadataSaveTextSG
 from .Simple_Readable_Metadata_VIDEO_SG import SimpleReadableMetadataVideoSG
+from .Simple_Readable_Metadata_Save_Prompt_SG import SavePositivePromptSG
+from .Simple_Readable_Metadata_Save_Prompt_SG import SaveNegativePromptSG
 
 NODE_CLASS_MAPPINGS = {
     "SimpleReadableMetadataMAXSG": SimpleReadableMetadataMAXSG,
     "SimpleReadableMetadataSG": SimpleReadableMetadataSG,
     "Simple Readable Metadata Text Viewer-SG": SimpleReadableMetadataTextViewerSG,
     "SimpleReadableMetadataSaveTextSG": SimpleReadableMetadataSaveTextSG,
-    "SimpleReadableMetadataVideoSG": SimpleReadableMetadataVideoSG
+    "SimpleReadableMetadataVideoSG": SimpleReadableMetadataVideoSG,
+    "SavePositivePromptSG": SavePositivePromptSG,
+    "SaveNegativePromptSG": SaveNegativePromptSG
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -18,7 +22,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "SimpleReadableMetadataSG": "Simple Readable Metadata-SG",
     "Simple Readable Metadata Text Viewer-SG": "Simple Readable Metadata 🧾 Text Viewer-SG",
     "SimpleReadableMetadataSaveTextSG": "Simple Readable Metadata Save Text-SG",
-    "SimpleReadableMetadataVideoSG": "Simple Readable Metadata (VIDEO)-SG"
+    "SimpleReadableMetadataVideoSG": "Simple Readable Metadata (VIDEO)-SG",
+    "SavePositivePromptSG": "Simple_Readable_Metadata_Save_Prompt (POSITIVE)-SG",
+    "SaveNegativePromptSG": "Simple_Readable_Metadata_Save_Prompt (NEGATIVE)-SG"
 }
 
 WEB_DIRECTORY = "./web"
